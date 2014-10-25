a86a8d1307469e41e7ec9f5f85901f956c7043df
Claudio <claudiospuy@gmail.com>
2014-10-25 17:51:29 -0200

    Added Featured page.

--- app/scripts/controllers/featured.js
+++ app/scripts/controllers/featured.js
@@ -4,4 +4,11 @@ angular.module('atenasApp')
   	.controller('FeaturedCtrl', function ($scope, realEstatesList) {
   		$scope.realEstates = realEstatesList.splice(0, 3);
       	console.info('FeaturedCtrl', $scope.realEstates);
+
+      	jQuery(function(){
+	        jQuery('#camera_wrap_1').camera({
+	          height: '500px',
+	          pagination: false,
+	        });
+	      });
   	});
