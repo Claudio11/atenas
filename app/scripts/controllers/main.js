3db87ca146da7ca11ec16c732d074f16db7a7228
Claudio <claudiospuy@gmail.com>
2014-11-01 21:02:15 -0200

    Merged footer, improved properties styles and filter styles.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,7 @@ angular.module('atenasApp')
     .controller('MainCtrl', function ($scope, $location) {
 
         $scope.$on('$routeChangeSuccess', function(next, current) {
-            $scope.isSearchable = $location.url() === '/list';
+            $scope.isList = $location.url() === '/list';
+            $scope.isFeatured = $location.url() === '/';
         });
     });
