1d79017e1b5ce602e4bb578b27ac8f28a6c2b61a
Claudio <claudiospuy@gmail.com>
2014-11-11 00:56:36 -0200

    Added grey background to selected menu.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,5 +7,6 @@ angular.module('atenasApp')
 	        $scope.$on('$routeChangeSuccess', function(next, current) {
 	            $scope.isList = $location.url() === '/list';
 	            $scope.isFeatured = $location.url() === '/';
+	            $scope.isContact = $location.url() === '/contact';
 	        });
 	    }]);
