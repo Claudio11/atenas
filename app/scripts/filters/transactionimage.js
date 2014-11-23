4a60d9e5a2b0430c79aac3014e3390b702654222
Claudio <claudiospuy@gmail.com>
2014-11-23 17:19:51 -0200

    Improved transaction image filter.

--- app/scripts/filters/transactionimage.js
+++ app/scripts/filters/transactionimage.js
@@ -2,7 +2,14 @@
 
 angular.module('atenasApp')
   .filter('transactionImage', function () {
-    return function (input) {
-      return input ? 'images/icon1.png' : 'images/icon11.png';
+    return function (property) {
+    	var transactionImage;
+    	if (property.sale && property.rent) {
+    		transactionImage = 'images/icon111.png'
+    	}
+    	else {
+    		transactionImage = property.sale ? 'images/icon1.png' : 'images/icon11.png';
+    	}
+        return transactionImage;
     };
   });
