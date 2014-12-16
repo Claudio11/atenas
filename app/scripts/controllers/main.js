d18bef94f2781be7614482eb02a93cdb64c3a592
Claudio <claudiospuy@gmail.com>
2014-12-16 00:25:15 -0200

    Small improvements.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,11 +5,16 @@ angular.module('atenasApp')
         function ($scope, $location) {
 
             $scope.currentDetail; // Current real estate in the detail popup.
+            $scope.menuOpen = false; // Menu list for small devices (TODO: also close it on resize).
 
             $scope.$on('$routeChangeSuccess', function(next, current) {
                 $scope.isList = $location.url() === '/list';
                 $scope.isFeatured = $location.url() === '/';
                 $scope.isContact = $location.url() === '/contact';
+
+                if ($scope.menuOpen) {
+                    $scope.toggleMenu();
+                }
             });
 
             // When the currentDetail changes, we send it to the server so it is set as viewed by the user (if last view is more than one day (TODO)). 
@@ -22,5 +27,10 @@ angular.module('atenasApp')
             // Sets the current detail real estate.
             $scope.setCurrentDetail = function (currentDetail) {
                 $scope.currentDetail = currentDetail;
+            }
+
+            // Toggles menu on small devices.
+            $scope.toggleMenu = function () {
+                $scope.menuOpen = !$scope.menuOpen;
             }
         }]);
