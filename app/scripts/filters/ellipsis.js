612ae04ced69f59a8c4ec436da67277fa1fd2ac0
Claudio <claudiospuy@gmail.com>
2015-01-10 22:17:42 -0200

    Added mailer and layout fixes.

--- app/scripts/filters/ellipsis.js
+++ app/scripts/filters/ellipsis.js
@@ -8,7 +8,7 @@ angular.module('atenasApp')
         if (typeof input !== 'undefined') {
             var currentWidth = $window.innerWidth;
             if (currentWidth < 1000) {
-                limit = Math.floor(limit * (3/4));  // In smaller devices text has more height (because of small width and fluid layout).
+                limit = Math.floor(limit * (2/3));  // In smaller devices text has more height (because of small width and fluid layout).
             }
             resultInput = (input.length < limit) ? input : $filter('limitTo')(input, limit - 2) +  '...';
         }
